ee79a4359ffbb343e6985009ad07d0db508b651e
Alexandre Justino <alexandretok@gmail.com>
2017-05-18 23:48:53 -0300

    Adiciona anti-flood

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import datetime
 import threading
+import Queue
+import time
 
 # Pequena gambiarra para possibilitar um objeto generico
 class Object(object):
@@ -21,9 +23,12 @@ listen_socket.listen(0)
 def threadDoCliente(cliente):
 	global listaClientes
 
+	# Guarda o tempo de envio das ultimas 5 mensagens enviadas pelo cliente
+	ultimasMensagens = Queue.Queue(5)
+
 	print str(len(listaClientes)) + " clientes conectados"
 
-	cliente.conn.send("Digite seu apelido: ");
+	cliente.conn.send("Digite seu apelido: ")
 	cliente.apelido = cliente.conn.recv(1024).replace("\r\n", "")
 	
 	broadcast(cliente.apelido + " entrou na sala.")
@@ -31,13 +36,21 @@ def threadDoCliente(cliente):
 	# Configura um timeout para saber quando o cliente encerrou a conexao
 	cliente.conn.settimeout(20)
 	disconnected = False
+	quintaUltima = None
 	while not disconnected:
 		try:
 			msg = cliente.conn.recv(1024).replace("\r\n", "")
 
-			# A mensagem ___heart_beat___ serve apenas para nao dar timeout
-			if msg != "___heart_beat___":
-				broadcast(cliente.apelido + ": " + msg)
+			if ultimasMensagens.full():
+				quintaUltima = ultimasMensagens.get(False)
+
+			if ultimasMensagens.qsize() == 4 and quintaUltima != None and (time.time() - quintaUltima) <= 2:
+				print "Stop the flood!"
+				cliente.conn.send("Stop the flood!")
+			elif msg != "___heart_beat___": # A mensagem ___heart_beat___ serve apenas para nao dar timeout
+				broadcast(cliente.apelido + " escreveu: " + msg)
+				print "mensagem recebida em " + str(time.time())
+				ultimasMensagens.put(time.time())
 		except socket.timeout:
 			disconnected = True
 
