579109554a5727f7fb5229c8e7a162f6cfa88c1f
Alexandre Justino <alexandretok@gmail.com>
2017-05-19 01:43:34 -0300

    Adiciona comandos no terminal

--- server.py
+++ server.py
@@ -10,23 +10,24 @@ class Object(object):
 
 # Constantes
 PORT = 6666
+TIMEOUT = 20
+ANTI_FLOOD_TIME_LIMIT = 2 # Tempo limite (em segundos)
+ANTI_FLOOD_MSG_QTD = 5 # Quantidade de mensagens que podem ser enviada dentro do tempo limite
 
 # Variaveis globais
 listaClientes = []
 
-# Configura socket
+# Configuracao do socket
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind(('', PORT))
 listen_socket.listen(0)
 
 def threadDoCliente(cliente):
-	global listaClientes
+	global listaClientes, TIMEOUT, ANTI_FLOOD_MSG_QTD, ANTI_FLOOD_TIME_LIMIT
 
-	# Guarda o tempo de envio das ultimas 5 mensagens enviadas pelo cliente
-	ultimasMensagens = Queue.Queue(5)
-
-	print str(len(listaClientes)) + " clientes conectados"
+	# Essa lista guarda o tempo de envio das ultimas 5 mensagens enviadas pelo cliente
+	ultimasMensagens = Queue.Queue(ANTI_FLOOD_MSG_QTD)
 
 	cliente.conn.send("Digite seu apelido: ")
 	cliente.apelido = cliente.conn.recv(1024).replace("\r\n", "")
@@ -34,22 +35,27 @@ def threadDoCliente(cliente):
 	broadcast(cliente.apelido + " entrou na sala.")
 
 	# Configura um timeout para saber quando o cliente encerrou a conexao
-	cliente.conn.settimeout(20)
+	cliente.conn.settimeout(TIMEOUT)
 	disconnected = False
 	quintaUltima = None
 	while not disconnected:
 		try:
-			msg = cliente.conn.recv(1024).replace("\r\n", "")
+			msg = cliente.conn.recv(1024)
+
+			# Se o cliente se desconectar inesperadamente, o socket recebe uma string vazia
+			if msg == "":
+				break
+
+			msg = msg.replace("\r\n", "")
 
 			if ultimasMensagens.full():
 				quintaUltima = ultimasMensagens.get(False)
 
-			if ultimasMensagens.qsize() == 4 and quintaUltima != None and (time.time() - quintaUltima) <= 2:
+			if ultimasMensagens.qsize() == 4 and quintaUltima != None and (time.time() - quintaUltima) <= ANTI_FLOOD_TIME_LIMIT:
 				print "Stop the flood!"
-				cliente.conn.send("Stop the flood!")
-			elif msg != "___heart_beat___": # A mensagem ___heart_beat___ serve apenas para nao dar timeout
+				cliente.conn.send("Stop the flood!\n")
+			elif msg != "__heart_beat__": # A mensagem ___heart_beat___ serve apenas para nao dar timeout
 				broadcast(cliente.apelido + " escreveu: " + msg)
-				print "mensagem recebida em " + str(time.time())
 				ultimasMensagens.put(time.time())
 		except socket.timeout:
 			disconnected = True
@@ -57,18 +63,36 @@ def threadDoCliente(cliente):
 	cliente.conn.close()
 	listaClientes.remove(cliente)
 	broadcast(cliente.apelido + " saiu da sala.")
-	print str(len(listaClientes)) + " clientes conectados"
+
+def recebeComandos():
+	while True:
+		global listaClientes
+		var = raw_input("""
+Lista de comandos:
+ listar (ls): apresenta uma lista dos usuarios conectados no momento
+
+""")
+		if var == "listar" or var == "ls":
+			if not len(listaClientes):
+				print "Nenhum cliente conectado."
+			for cliente in listaClientes:
+				print "<" + (cliente.apelido if hasattr(cliente, "apelido") else "_EMPTY_") + ", " + str(cliente.addr[0]) + ", " + str(cliente.addr[1]) + ">"
+	
 
 # Envia uma string para todos os clientes conectados
 def broadcast(string):
 	global listaClientes
 	now = datetime.datetime.now();
 	for cliente in listaClientes:
-		cliente.conn.send(str(now.hour) + ":"+ str(now.minute) + ":" + str(now.second) + " - " + string + "\n");
+		try:
+			cliente.conn.send(str(now.hour) + ":"+ str(now.minute) + ":" + str(now.second) + " - " + string + "\n");
+		except:
+			time.sleep(0)
 
 # Main loop
 
 threadID = 0
+threading.Thread(target=recebeComandos).start()
 
 while True:
 	# Fica preso aqui enquanto nao receber uma nova conexao
@@ -77,6 +101,7 @@ while True:
 	tmp = Object()
 	tmp.id = threadID
 	tmp.conn = client_connection
+	tmp.addr = client_address
 
 	listaClientes.append(tmp)
 	
